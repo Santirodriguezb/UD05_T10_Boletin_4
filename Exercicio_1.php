b26b77ff1281fcbd242b03dcb0507f14bb17006b
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-02-18 09:41:41 +0100

    Exercicio 1

---
+++ Exercicio_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=+, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+    <?php
+        for ($i=0; $i <100 ; $i++) { 
+            if ($i%5==0) {
+                echo "<p>$i</p>";
+            }
+        }
+
+    ?>
+</body>
+</html>

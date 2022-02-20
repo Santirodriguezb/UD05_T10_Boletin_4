a5e5eb7eef26706802f8b192055daa9fa2f4a22c
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-02-20 11:34:24 +0100

    Exercicio 2

---
+++ Exercicio_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 2</title>
+</head>
+<body>
+    <?php
+        $x=0;
+        while ($x <= 100) {
+            $multiplicacion=$x * 5 ;
+            echo "<p>5*$x da un resultado de = $multiplciacion </p>";
+            $x+=1;
+        }
+
+
+
+    ?>
+</body>
+</html>

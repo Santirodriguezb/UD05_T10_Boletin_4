026cff073a6fc51a34c604337db4ff209f326eff
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-02-20 11:41:41 +0100

    Exercicio 4

---
+++ Exercicio_4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 4</title>
+</head>
+<body>
+    <?php
+        for ($i=0; $i <=8 ; $i++) { 
+            $multiplicacion = $i * 20;
+            $x=320 - $multiplicacion;
+
+            echo "<p>Los numeros son: $x </p>";
+        }
+
+
+
+
+    ?>
+</body>
+</html>
